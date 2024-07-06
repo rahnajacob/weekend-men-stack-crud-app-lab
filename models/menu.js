f657b5606f9069ea5f89a8c7b9155fd0beaee40b
rjacob <rahnarjacob@gmail.com>
2024-07-07 02:59:02 +0530

    Added index, add, show routes + html. Edit route + ejs left to do

--- models/menu.js
+++ models/menu.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose")
 
-const ingredientSchema = new mongoose.Schema({
-    base: String,
-    binder: String,
-    spices: [String],
-    oil: String,
-    protein: String,
-})
+
 
 const menuSchema = new mongoose.Schema({
     dish: { type: String, required: true },
     timeOfDay: { type: String, required: true },
     prepTime: Number,
-    ingredients: ingredientSchema
+    prepcomplete: Boolean,
+    date: {type: Date, required: true}
 })
 
 const Menu = mongoose.model("Menu", menuSchema)
-const Ingredients = mongoose.model("Ingredients", ingredientSchema)
 
-module.exports = {Menu: Menu, Ingredients: Ingredients}
+module.exports = Menu
+
+//Code cemetary
+
+// const ingredientSchema = new mongoose.Schema({
+//     base: String,
+//     binder: String,
+//     spices: [String],
+//     oil: String,
+//     protein: String,
+// })
+
+// ingredients: ingredientSchema
+
+// const Ingredients = mongoose.model("Ingredients", ingredientSchema)
+
+// module.exports = {Menu: Menu, Ingredients: Ingredients}
--- server.js
+++ server.js
@@ -9,12 +9,69 @@ const methodOverride = require("method-override")
 //CONSTANTS
 const app = express()
 const Menu = require("./models/menu.js")
-const Ingredients = require("./models/menu.js")
+// const Ingredients = require("./models/menu.js")
 
 //MIDDLEWARE
 app.use(morgan("dev"))
 app.use(methodOverride("_method"))
 app.use(express.urlencoded({extended:true}))
+
+//ROUTES
+
+//local host landing page
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+//new item form
+app.get("/menu/new", async (req,res) => {
+    res.render("menu/new.ejs")
+})
+
+//get form and add to database
+app.post("/menu", async (req,res) => {
+    req.body.prepcomplete = Boolean(req.body.prepcomplete)
+    await Menu.create(req.body)
+    res.redirect("/menu")
+})
+
+//get menu index page
+app.get("/menu", async (req, res) => {
+    const fullMenu = await Menu.find({})
+    res.render("menu/index.ejs", {menu:fullMenu})
+})
+
+//show menu object details page
+app.get("/menu/:menuId", async (req, res) => {
+    const dishItem = await Menu.findById(req.params.menuId)
+    res.render("menu/show.ejs", {item:dishItem})
+})
+
+//delete an object
+app.delete("/menu/:menuId", async (req, res) => {
+    await Menu.findByIdAndDelete(req.params.menuId)
+    res.redirect("/menu")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //SERVER CONNECTIONS
 const connect = async() => {
